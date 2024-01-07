3744144d8ff163388368a3cdbe394c47a7457d48
Ekii0 <ekiio@protonmail.com>
2024-01-07 18:04:56 +0100

    modifications for deployment to fly.io

--- index.js
+++ index.js
@@ -122,69 +122,69 @@ client.on("messageCreate", (message) => {
               symbol = "N/A";
             }
 
-            const uri = json[0]?.offChainMetadata?.uri;
+            const uri = json[0]?.onChainMetadata?.metadata?.data?.uri;
             console.log(`Name: ${name} | Symbol: ${symbol}`);
 
-            fetch(uri)
-              .then((res) => res.json())
-              .then((json) => {
-                let description = json.description;
-                if (description == undefined || description == "") {
-                  description = "N/A";
-                }
-                let img_uri = json.image;
-                if (img_uri.startsWith("data:image")) {
-                  img_uri = "attachment:" + img_uri;
-                }
-                if (
-                  !img_uri.startsWith("http") &&
-                  !img_uri.startsWith("data:image")
-                ) {
-                  img_uri =
-                    "https://icons.veryicon.com/png/o/education-technology/alibaba-cloud-iot-business-department/image-load-failed.png";
-                }
+            // fetch(uri)
+            //   .then((res) => res.json())
+            //   .then((json) => {
+            let description = json[0]?.offChainMetadata?.metadata?.description;
+            if (description == undefined || description == "") {
+              description = "N/A";
+            }
+            let img_uri = json[0]?.offChainMetadata?.metadata?.image;
+            if (img_uri.startsWith("data:image")) {
+              img_uri = "attachment:" + img_uri;
+            }
+            if (
+              !img_uri.startsWith("http") &&
+              !img_uri.startsWith("data:image")
+            ) {
+              img_uri =
+                "https://icons.veryicon.com/png/o/education-technology/alibaba-cloud-iot-business-department/image-load-failed.png";
+            }
 
-                console.log(`Description: ${description} | Image: ${img_uri}`);
+            console.log(`Description: ${description} | Image: ${img_uri}`);
 
-                const embed = new EmbedBuilder()
-                  .setColor(0x4f30ff)
-                  .setAuthor({ name: "LaunchBot" })
-                  .setTitle("Launch Alert")
-                  .setURL(`https://solscan.io/account/${ammId}`)
-                  .setDescription(`Launch Alert for ${name}`)
-                  .setThumbnail(img_uri)
-                  .addFields(
-                    { name: "Token Name:", value: name, inline: true },
-                    { name: "Symbol: ", value: symbol, inline: true },
-                    {
-                      name: "Launch Time: ",
-                      value: launchTime,
-                      inline: true,
-                    },
-                    {
-                      name: "Description: ",
-                      value: description,
-                      inline: false,
-                    },
-                    {
-                      name: "Pooled SOL: ",
-                      value: pcAmount,
-                      inline: false,
-                    },
-                    {
-                      name: "Contract Address: ",
-                      value: `[${tokenMint}](https://dexscreener.com/solana/${tokenMint} "DexScreener")`,
-                      inline: false,
-                    }
-                  )
-                  .setImage(img_uri)
-                  .setTimestamp();
+            const embed = new EmbedBuilder()
+              .setColor(0x4f30ff)
+              .setAuthor({ name: "LaunchBot" })
+              .setTitle("Launch Alert")
+              .setURL(`https://solscan.io/account/${ammId}`)
+              .setDescription(`Launch Alert for ${name}`)
+              .setThumbnail(img_uri)
+              .addFields(
+                { name: "Token Name:", value: name, inline: true },
+                { name: "Symbol: ", value: symbol, inline: true },
+                {
+                  name: "Launch Time: ",
+                  value: launchTime,
+                  inline: true,
+                },
+                {
+                  name: "Description: ",
+                  value: description,
+                  inline: false,
+                },
+                {
+                  name: "Pooled SOL: ",
+                  value: pcAmount,
+                  inline: false,
+                },
+                {
+                  name: "Contract Address: ",
+                  value: `[${tokenMint}](https://dexscreener.com/solana/${tokenMint} "DexScreener")`,
+                  inline: false,
+                }
+              )
+              .setImage(img_uri)
+              .setTimestamp();
 
-                const targetChannel = client.channels.cache.get(
-                  "1192767457300054067"
-                );
-                targetChannel.send({ embeds: [embed] });
-              });
+            const targetChannel = client.channels.cache.get(
+              "1192767457300054067"
+            );
+            targetChannel.send({ embeds: [embed] });
+            // });
           });
       });
   }
